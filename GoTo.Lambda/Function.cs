c53e4661415b05ec5f4ff8e9cfac9c393b3449a4
Martin Hochstrasser <highstreeto@users.noreply.github.com>
2019-05-12 13:43:27 +0200

    Add project and initial setup

---
+++ GoTo.Lambda/Function.cs
@@ -0,0 +1,19 @@
+using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Response;
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace GoTo.Lambda
+{
+    public class Function
+    {
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
+        {
+            // TODO Logic here
+            return ResponseBuilder.Tell("Ok");
+        }
+    }
+}
